ff861a5394cb61f949a62bd7fc44af699e184062
Victor Martin <victor@vmleon.es>
2016-08-04 21:22:55 +0100

    Add about page

--- app.js
+++ app.js
@@ -12,6 +12,12 @@
     res.send('Home Mealdowlark Travel');
   });
 
+  app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.status(200);
+    res.send('About Mealdowlark Travel');
+  });
+
   app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,17 +5,32 @@ var base_url = "http://localhost:3000/";
 
 describe('Testing routes to resources', function() {
   describe('Home page', function() {
+    var url = base_url + "";
     it('should return 200 status', function(done) {
-      var url = base_url + "";
       request(url, (err, response) => {
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
     it('should contain "Home Mealdowlark Travel" in the response body', function(done) {
-      var url = base_url + "";
       request(url, (err, response, body) => {
         expect(body).to.equal('Home Mealdowlark Travel');
+        done();
+      });
+    });
+  });
+
+  describe('About page', function() {
+    var url = base_url + "about";
+    it('should return 200 status', function(done) {
+      request(url, (err, response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it('should contain "About Mealdowlark Travel" in the response body', function(done) {
+      request(url, (err, response, body) => {
+        expect(body).to.equal('About Mealdowlark Travel');
         done();
       });
     });
@@ -24,15 +39,14 @@ describe('Testing routes to resources', function() {
 
 describe('Testing error routes', function() {
   describe('Resource not found', function() {
+    var url = base_url + "notfound";
     it('should return 404 status', function(done) {
-      var url = base_url + "notfound";
       request(url, (err, response) => {
         expect(response.statusCode).to.equal(404);
         done();
       });
     });
     it('should contain "404 - Not found" in the response body', function(done) {
-      var url = base_url + "notfound";
       request(url, (err, response, body) => {
         expect(body).to.equal('404 - Not found');
         done();
